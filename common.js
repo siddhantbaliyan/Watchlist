bcb61e510bb337e87979b53a51c76ad1bc4d0d96
siddhantbaliyan <52606880+siddhantbaliyan@users.noreply.github.com>
2026-03-11 15:22:08 +0530

    Add files via upload

---
+++ common.js
@@ -0,0 +1,6 @@
+// common behavior for all pages
+// currently only the dark mode toggle, but you can add more shared functions here
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
