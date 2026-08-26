70c64aa60178d50dadc360a2fcf9f1b8058fe7db
Siddhant Baliyan <52606880+siddhantbaliyan@users.noreply.github.com>
2026-08-26 11:02:37 +0530

    Update TV shows list with new entries
    Removed some TV shows from the list and added new ones.

--- tvshows.js
+++ tvshows.js
@@ -9,16 +9,17 @@ const tvShows = [
     { name: "Dark", seasons: 3, episodes: 26, watched: true, continueFrom: null },
     { name: "Sherlock", seasons: 4, episodes: 13, watched: true, continueFrom: null },
     { name: "Suits", seasons: 9, episodes: 134, watched: true, continueFrom: null },
-    { name: "I am Groot", seasons: 2, episodes: 10, watched: true, continueFrom: null },
-    { name: "The Punisher", seasons: 2, episodes: 26, watched: true, continueFrom: null },
-    { name: "Moon Knight", seasons: 1, episodes: 6, watched: true, continueFrom: null },
     { name: "Wednesday", seasons: 1, episodes: 8, watched: true, continueFrom: null },
     { name: "Jack Ryan", seasons: 4, episodes: 30, watched: true, continueFrom: null },
     { name: "The Queen's Gambit", seasons: 1, episodes: 7, watched: true, continueFrom: null },
     { name: "Band of Brothers", seasons: 1, episodes: 10, watched: true, continueFrom: null },
-    { name: "Loki", seasons: 2, episodes: 12, watched: true, continueFrom: null },
     { name: "That 90's Show", seasons: 3, episodes: 26, watched: true, continueFrom: null },
     { name: "That 70's Show", seasons: 3, episodes: 26, watched: true, continueFrom: null },
+    { name: "The Wire", seasons: 5, episodes: 60, watched: true, continueFrom: null },
+    { name: "I am Groot", seasons: 2, episodes: 10, watched: true, continueFrom: null },
+    { name: "The Punisher", seasons: 2, episodes: 26, watched: true, continueFrom: null },
+    { name: "Moon Knight", seasons: 1, episodes: 6, watched: true, continueFrom: null },
+    { name: "Loki", seasons: 2, episodes: 12, watched: true, continueFrom: null },
     { name: "Cobra Kai", seasons: 6, episodes: 65, watched: false, continueFrom: "S5E7" },
     { name: "IT Crowd", seasons: 4, episodes: 24, watched: false, continueFrom: "S1E4" },
     { name: "Brooklyn Nine-Nine", seasons: 8, episodes: 153, watched: false, continueFrom: "S2E15" },
@@ -37,7 +38,9 @@ const tvShows = [
     { name: "You", seasons: 4, episodes: 40, watched: false, continueFrom: null },
     { name: "Emily in Paris", seasons: 4, episodes: 40, watched: false, continueFrom: "S2E1" },
     { name: "The Sopranos", seasons: 6, episodes: 86, watched: false, continueFrom: null },
-    { name: "The Wire", seasons: 5, episodes: 60, watched: false, continueFrom: null },
+    { name: "dexter", seasons: 8, episodes: 96, watched: false, continueFrom: null },
+    { name: "The Mentalist", seasons: 7, episodes: 151, watched: false, continueFrom: null },
+   
 ];
 
 const tvTableBody = document.getElementById('tvTableBody');
